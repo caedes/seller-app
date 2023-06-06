18c5379c067cba0b0fd21fc39f0f062d29db4899
Romain Laurent <romain.laurent@cdiscount.com>
2023-06-06 17:37:24 +0200

    feat: 🎸 update Header into DS, add Menu to DS

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,22 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import Header from "../components/Header";
+import { Header, Menu } from "../ds/molecules";
+
+const items = [
+  { name: "Produits", route: "/products" },
+  { name: "Commandes", route: "/commandes" },
+  { name: "Finance", route: "/finance" },
+  { name: "Rapports", route: "/rapports" },
+  { name: "Messages", route: "/messages" },
+  { name: "Marketplaces", route: "/marketplaces" },
+];
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header title="Marjane" caption="Bienvenue" />
+      <Menu items={items} />
       MAIN
     </ThemeProvider>
   );
--- src/ds/molecules/Header/index.js
+++ src/ds/molecules/Header/index.js
@@ -1,20 +1,26 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
+import { string } from "prop-types";
 
-export default function Header() {
+export default function Header({ title, caption }) {
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h5" component="h1">
-          SellerApp
+          {title}
         </Typography>
         <Typography
           sx={{
             paddingLeft: 2,
           }}
         >
-          Bienvenue
+          {caption}
         </Typography>
       </Toolbar>
     </AppBar>
   );
 }
+
+Header.propTypes = {
+  title: string,
+  caption: string,
+};
