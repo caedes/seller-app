af205653c874357499d4a8b42009a84dc5e85bcb
Romain Laurent <romain.laurent@cdiscount.com>
2023-06-07 15:28:07 +0200

    feat: 🎸 add the products DataGrid in ProdutcsPage

---
+++ src/ds/atoms/ProductImage/index.js
@@ -0,0 +1,13 @@
+import { Image } from "mui-image";
+import { shape, string } from "prop-types";
+
+export default function ProductImage({ row }) {
+  return <Image src={row.imageUrl} alt={row.name} width={150} duration={250} />;
+}
+
+ProductImage.propTypes = {
+  row: shape({
+    imageUrl: string,
+    name: string,
+  }),
+};
